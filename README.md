1017c1e0094c893d6b00b42cca4420c6be0efeab
Craig Myles <cr@igmyles.com>
2018-08-29 13:24:04 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Instant Firestore Utils
+
+Firestore helper methods.
+
+_**Please note:** This is an unofficial Firestore package, designed to assist with common Firestore tasks_
+
+## Features
+
+- Assign document id
+- Serialize references
+- Serialize Firestore document
+
+## Installation
+
+You can install this wrapper by running the following in your project:
+
+```bash
+$ npm install instant-firestore-utils
+```
+
+## Usage
+
+**ES Modules**
+
+```javascript
+import { serializeDocument } from 'instant-firestore-utils';
+
+// Do stuff with serializeDocument or any of the other Utils you import
+```
+
+**CommonJS Modules**
+
+```javascript
+const utils = require('instant-firestore-utils');
+
+// Do stuff with serializeDocument or any of the other Utils you import
+```
+
+## Running Tests
+
+To run the tests, clone the repository and install the dependencies:
+
+```bash
+git clone https://github.com/JSJInvestments/instant-firestore-utils.git
+cd instant-utils && npm i
+npm run test
+```
+
+## License
+
+[MIT](LICENSE)
---
+++ src/index.js
@@ -0,0 +1,82 @@
+import { asyncForEach } from 'instant-utils';
+
+/**
+ * Get document data and assign an id (prevents overriding the `id` field in the data if one exists)
+ * @param {object} doc Firestore document
+ */
+export function getDocumentData(doc) {
+  try {
+    if (doc && doc.exists) {
+      // Serialize the document
+      let data = doc.data();
+
+      // Assign an id
+      if (data.id) {
+        data._id = doc.id;
+      } else {
+        data.id = doc.id;
+      }
+
+      return data;
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+/**
+ * Serialize document references
+ * @param {object} data Document data
+ * @param {object} options Options
+ */
+export async function serializeDocumentReferences(data, options = {}) {
+  try {
+    let populate;
+    let rest;
+    if (options.populate) {
+      const split = options.populate.split('.');
+      populate = split.shift();
+      rest = split.length > 0 ? split.join('.') : null;
+    }
+
+    await asyncForEach(Object.keys(data), async key => {
+      if (
+        typeof data[key] === 'object' &&
+        typeof data[key].get === 'function'
+      ) {
+        if (key === populate) {
+          const doc = await data[key].get();
+          data[key] = await serializeDocument(doc, {
+            ...options,
+            populate: rest,
+          });
+        } else if (options.hideUnpopulatedReferences) {
+          delete data[key];
+        }
+      }
+    });
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+/**
+ * Serialize a Firestore document
+ * @param {object} doc Firestore document
+ * @param {object} options Options
+ */
+export async function serializeDocument(doc, options = {}) {
+  try {
+    if (doc && doc.exists) {
+      let data = getDocumentData(doc);
+
+      // Serialize document references
+      data = serializeDocumentReferences(data, options);
+
+      return data;
+    }
+  } catch (error) {
+    throw error;
+  }
+}
